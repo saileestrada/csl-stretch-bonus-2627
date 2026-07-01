de949293b61ba912fdf1390dba8871887828fd0b
saileestrada <saileestrada1691@gmail.com>
2026-07-01 13:25:20 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // Paste your Google Apps Script Web App URL here after deploying it.
 // See APPS_SCRIPT.md for the deployment steps.
-export const SUBMIT_URL = export const SUBMIT_URL = 'https://script.google.com/macros/s/AKfycbzZH6LxSUppzAccIx2DGK0QsiBqjzj9FtJEEK2RPH4tAEmCon-0BqPZ3_mqNzwIamQiRA/exec';
+export const SUBMIT_URL = 'https://script.google.com/macros/s/AKfycbzZH6LxSUppzAccIx2DGK0QsiBqjzj9FtJEEK2RPH4tAEmCon-0BqPZ3_mqNzwIamQiRA/exec';
