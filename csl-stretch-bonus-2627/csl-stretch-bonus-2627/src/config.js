0c44e7769b706586f54e810285996d49d6c2915e
saileestrada <saileestrada1691@gmail.com>
2026-07-01 13:11:20 -0500

    Delete csl-stretch-bonus-2627/csl-stretch-bonus-2627 directory

--- csl-stretch-bonus-2627/csl-stretch-bonus-2627/src/config.js
+++
@@ -1,3 +0,0 @@
-// Paste your Google Apps Script Web App URL here after deploying it.
-// See APPS_SCRIPT.md for the deployment steps.
-export const SUBMIT_URL = 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE';
